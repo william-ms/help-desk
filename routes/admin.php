782d75de05367d975f39b590ff075e35636d4e8a
William Macedo Sartini <williamsartinidev@outlook.com>
2024-10-28 21:11:39 -0300

    Parâmetros das rotas corrigidas

--- routes/admin.php
+++ routes/admin.php
@@ -21,29 +21,29 @@ Route::name('admin.')->middleware('auth')->group(function() {
         Route::get('/', [MenuCategoryController::class, 'index'])->name('index')->can('menu_category.index');
         Route::get('/create', [MenuCategoryController::class, 'create'])->name('create')->can('menu_category.create');
         Route::post('/', [MenuCategoryController::class, 'store'])->name('store')->can('menu_category.create');
-        Route::get('/edit/{menu_category}', [MenuCategoryController::class, 'edit'])->name('edit')->can('menu_category.edit');
+        Route::get('/{menu_category}/edit', [MenuCategoryController::class, 'edit'])->name('edit')->can('menu_category.edit');
         Route::put('/{menu_category}', [MenuCategoryController::class, 'update'])->name('update')->can('menu_category.edit');
         Route::delete('/{menu_category}', [MenuCategoryController::class, 'destroy'])->name('destroy')->can('menu_category.destroy');
         Route::post('/order', [MenuCategoryController::class, 'order'])->name('order')->can('menu_category.order');
-        Route::get('/restore/{menu_category}', [MenuCategoryController::class, 'restore'])->name('restore')->can('menu_category.restore');
+        Route::get('/{menu_category}/restore', [MenuCategoryController::class, 'restore'])->name('restore')->can('menu_category.restore');
     });
 
     Route::group(['prefix' => 'menu', 'as' => 'menu.'], function() {
         Route::get('/', [MenuController::class, 'index'])->name('index')->can('menu.index');
         Route::get('/create', [MenuController::class, 'create'])->name('create')->can('menu.create');
         Route::post('/', [MenuController::class, 'store'])->name('store')->can('menu.create');
-        Route::get('/edit/{menu}', [MenuController::class, 'edit'])->name('edit')->can('menu.edit');
+        Route::get('/{menu}/edit', [MenuController::class, 'edit'])->name('edit')->can('menu.edit');
         Route::put('/{menu}', [MenuController::class, 'update'])->name('update')->can('menu.edit');
         Route::delete('/{menu}', [MenuController::class, 'destroy'])->name('destroy')->can('menu.destroy');
         Route::post('/order', [MenuController::class, 'order'])->name('order')->can('menu.order');
-        Route::get('/restore/{menu}', [MenuController::class, 'restore'])->name('restore')->can('menu.restore');
+        Route::get('/{menu}/restore', [MenuController::class, 'restore'])->name('restore')->can('menu.restore');
     });
 
     Route::group(['prefix' => 'permission', 'as' => 'permission.'], function() {
         Route::get('/', [PermissionController::class, 'index'])->name('index')->can('permission.index');
         Route::get('/create', [PermissionController::class, 'create'])->name('create')->can('permission.create');
         Route::post('/', [PermissionController::class, 'store'])->name('store')->can('permission.create');
-        Route::get('/edit/{permission}', [PermissionController::class, 'edit'])->name('edit')->can('permission.edit');
+        Route::get('/{permission}/edit', [PermissionController::class, 'edit'])->name('edit')->can('permission.edit');
         Route::put('/{permission}', [PermissionController::class, 'update'])->name('update')->can('permission.edit');
         Route::delete('/{permission}', [PermissionController::class, 'destroy'])->name('destroy')->can('permission.destroy');
     });
@@ -52,7 +52,7 @@ Route::name('admin.')->middleware('auth')->group(function() {
         Route::get('/', [RoleController::class, 'index'])->name('index')->can('role.index');
         Route::get('/create', [RoleController::class, 'create'])->name('create')->can('role.create');
         Route::post('/', [RoleController::class, 'store'])->name('store')->can('role.create');
-        Route::get('/edit/{role}', [RoleController::class, 'edit'])->name('edit')->can('role.edit');
+        Route::get('/{role}/edit', [RoleController::class, 'edit'])->name('edit')->can('role.edit');
         Route::put('/{role}', [RoleController::class, 'update'])->name('update')->can('role.edit');
         Route::delete('/{role}', [RoleController::class, 'destroy'])->name('destroy')->can('role.destroy');
     });
@@ -61,50 +61,50 @@ Route::name('admin.')->middleware('auth')->group(function() {
         Route::get('/', [UserController::class, 'index'])->name('index')->can('user.index');
         Route::get('/create', [UserController::class, 'create'])->name('create')->can('user.create');
         Route::post('/', [UserController::class, 'store'])->name('store')->can('user.create');
-        Route::get('/edit/{user}', [UserController::class, 'edit'])->name('edit')->can('user.edit');
+        Route::get('/{user}/edit', [UserController::class, 'edit'])->name('edit')->can('user.edit');
         Route::put('/{user}', [UserController::class, 'update'])->name('update')->can('user.edit');
         Route::delete('/{user}', [UserController::class, 'destroy'])->name('destroy')->can('user.destroy');
-        Route::get('/restore/{user}', [UserController::class, 'restore'])->name('restore')->can('user.restore');
+        Route::get('/{user}/restore', [UserController::class, 'restore'])->name('restore')->can('user.restore');
     });
 
     Route::group(['prefix' => 'company', 'as' => 'company.'], function() {
         Route::get('/', [CompanyController::class, 'index'])->name('index')->can('company.index');
         Route::get('/create', [CompanyController::class, 'create'])->name('create')->can('company.create');
         Route::post('/', [CompanyController::class, 'store'])->name('store')->can('company.create');
-        Route::get('/edit/{company}', [CompanyController::class, 'edit'])->name('edit')->can('company.edit');
+        Route::get('/{company}/edit', [CompanyController::class, 'edit'])->name('edit')->can('company.edit');
         Route::put('/{company}', [CompanyController::class, 'update'])->name('update')->can('company.edit');
         Route::delete('/{company}', [CompanyController::class, 'destroy'])->name('destroy')->can('company.destroy');
-        Route::get('/restore/{company}', [CompanyController::class, 'restore'])->name('restore')->can('company.restore');
+        Route::get('/{company}/restore', [CompanyController::class, 'restore'])->name('restore')->can('company.restore');
     });
 
     Route::group(['prefix' => 'departament', 'as' => 'departament.'], function() {
         Route::get('/', [DepartamentController::class, 'index'])->name('index')->can('departament.index');
         Route::get('/create', [DepartamentController::class, 'create'])->name('create')->can('departament.create');
         Route::post('/', [DepartamentController::class, 'store'])->name('store')->can('departament.create');
-        Route::get('/edit/{departament}', [DepartamentController::class, 'edit'])->name('edit')->can('departament.edit');
+        Route::get('/{departament}/edit', [DepartamentController::class, 'edit'])->name('edit')->can('departament.edit');
         Route::put('/{departament}', [DepartamentController::class, 'update'])->name('update')->can('departament.edit');
         Route::delete('/{departament}', [DepartamentController::class, 'destroy'])->name('destroy')->can('departament.destroy');
-        Route::get('/restore/{departament}', [DepartamentController::class, 'restore'])->name('restore')->can('departament.restore');
+        Route::get('/{departament}/restore', [DepartamentController::class, 'restore'])->name('restore')->can('departament.restore');
     });
 
     Route::group(['prefix' => 'category', 'as' => 'category.'], function() {
         Route::get('/', [CategoryController::class, 'index'])->name('index')->can('category.index');
         Route::get('/create', [CategoryController::class, 'create'])->name('create')->can('category.create');
         Route::post('/', [CategoryController::class, 'store'])->name('store')->can('category.create');
-        Route::get('/edit/{category}', [CategoryController::class, 'edit'])->name('edit')->can('category.edit');
+        Route::get('/{category}/edit', [CategoryController::class, 'edit'])->name('edit')->can('category.edit');
         Route::put('/{category}', [CategoryController::class, 'update'])->name('update')->can('category.edit');
         Route::delete('/{category}', [CategoryController::class, 'destroy'])->name('destroy')->can('category.destroy');
-        Route::get('/restore/{category}', [CategoryController::class, 'restore'])->name('restore')->can('category.restore');
+        Route::get('/{category}/restore', [CategoryController::class, 'restore'])->name('restore')->can('category.restore');
     });
 
     Route::group(['prefix' => 'subcategory', 'as' => 'subcategory.'], function() {
         Route::get('/', [SubcategoryController::class, 'index'])->name('index')->can('subcategory.index');
         Route::get('/create', [SubcategoryController::class, 'create'])->name('create')->can('subcategory.create');
         Route::post('/', [SubcategoryController::class, 'store'])->name('store')->can('subcategory.create');
-        Route::get('/edit/{subcategory}', [SubcategoryController::class, 'edit'])->name('edit')->can('subcategory.edit');
+        Route::get('/{subcategory}/edit', [SubcategoryController::class, 'edit'])->name('edit')->can('subcategory.edit');
         Route::put('/{subcategory}', [SubcategoryController::class, 'update'])->name('update')->can('subcategory.edit');
         Route::delete('/{subcategory}', [SubcategoryController::class, 'destroy'])->name('destroy')->can('subcategory.destroy');
-        Route::get('/restore/{subcategory}', [SubcategoryController::class, 'restore'])->name('restore')->can('subcategory.restore');
+        Route::get('/{subcategory}/restore', [SubcategoryController::class, 'restore'])->name('restore')->can('subcategory.restore');
     });
 
     Route::get('/log', [LogController::class, 'index'])->name('log.index')->can('log.index');;
